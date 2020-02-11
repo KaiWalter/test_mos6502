f4ea3d5af92b6f547e7e527b42373015a9820949
Kai Walter <kai.walter@zeiss.com>
2020-02-11 17:53:07 +0100

    test ciruclating in a particular block

--- test_mos6502.cpp
+++ test_mos6502.cpp
@@ -9,6 +9,8 @@
 
 uint8_t ram[0x10000];
 bool execute = true;
+uint16_t maxpc = 0;
+uint16_t minpc = 0xFFFF;
 
 uint8_t MemoryRead(uint16_t address)
 {
@@ -22,19 +24,35 @@ void MemoryWrite(uint16_t address, uint8_t value)
 
 void StatusWrite(uint16_t pc, uint8_t sp, uint8_t status, uint8_t A, uint8_t X, uint8_t Y, uint8_t opcode, bool illegalOpcode)
 {
+	if (pc > maxpc)
+		maxpc = pc;
+
+	if (pc < minpc)
+		minpc = pc;
+
+	if (opcode == 0x40 || opcode == 0x60)
+	{
+		maxpc = 0;
+		minpc = 0xFFFF;
+	}
+
 	cout << "PC:";
 	cout << std::setw(4) << std::setfill('0') << std::hex << (int)pc;
-	cout << " SP:";
+	cout << " MINPC:";
+	cout << std::setw(4) << std::setfill('0') << std::hex << (int)minpc;
+	cout << " MAXPC:";
+	cout << std::setw(4) << std::setfill('0') << std::hex << (int)maxpc;
+	cout << " | SP:";
 	cout << std::setw(2) << std::hex << (int)sp;
-	cout << " STATUS:";
+	cout << " | STATUS:";
 	cout << std::setw(2) << std::hex << (int)status;
-	cout << " A:";
+	cout << " | A:";
 	cout << std::setw(2) << std::hex << (int)A;
 	cout << " X:";
 	cout << std::setw(2) << std::hex << (int)X;
 	cout << " Y:";
 	cout << std::setw(2) << std::hex << (int)Y;
-	cout << " Op:";
+	cout << " | Op:";
 	cout << std::setw(2) << std::hex << (int)opcode << endl;
 	execute = !illegalOpcode;
 }
