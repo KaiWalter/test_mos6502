a1b5ad53e6866c8dfce54b986d7d61f2b39ff44b
Kai Walter <kai.walter@zeiss.com>
2020-02-12 07:26:43 +0100

    successful test

--- test_mos6502.cpp
+++ test_mos6502.cpp
@@ -1,4 +1,6 @@
 
+#include <chrono>
+#include <ctime>
 #include <cstdint>
 #include <iostream>
 #include <iomanip>
@@ -10,8 +12,10 @@
 uint8_t ram[0x10000];
 bool execute = true;
 uint16_t maxpc = 0;
+uint16_t prevpc = 0;
 
-#define END_OF_MAIN 0x346c
+//#define DETAILED
+#define END_OF_MAIN 0x3469
 
 uint8_t MemoryRead(uint16_t address)
 {
@@ -25,12 +29,40 @@ void MemoryWrite(uint16_t address, uint8_t value)
 
 void StatusWrite(uint16_t pc, uint8_t sp, uint8_t status, uint8_t A, uint8_t X, uint8_t Y, uint8_t opcode, bool illegalOpcode)
 {
-	if (pc > maxpc && pc < END_OF_MAIN)
+	if (pc > maxpc&& pc < END_OF_MAIN)
+	{
 		maxpc = pc;
+#ifndef DETAILED
+		auto givemetime = chrono::system_clock::to_time_t(chrono::system_clock::now());
+		char printtime[30];
+		ctime_s(printtime, 30, &givemetime);
+		cout << " MAXPC:";
+		cout << std::setw(4) << std::setfill('0') << std::hex << (int)maxpc << ":" << printtime;
+#endif // !DETAILED
+
+	}
 
 	if (pc == END_OF_MAIN)
+	{
+		execute = false;
+		cout << "PC:";
+		cout << std::setw(4) << std::setfill('0') << std::hex << (int)pc;
+		cout << " ** end of main" << endl;
+	}
+	else if (pc == prevpc)
+	{
+		execute = false;
+		cout << "PC:";
+		cout << std::setw(4) << std::setfill('0') << std::hex << (int)pc;
+		cout << " | Op:";
+		cout << std::setw(2) << std::hex << (int)opcode;
+		cout << " !! loop" << endl;
+	}
+
+	if (illegalOpcode)
 		execute = false;
 
+#if DETAILED
 	cout << "PC:";
 	cout << std::setw(4) << std::setfill('0') << std::hex << (int)pc;
 	cout << " MAXPC:";
@@ -48,14 +80,14 @@ void StatusWrite(uint16_t pc, uint8_t sp, uint8_t status, uint8_t A, uint8_t X, 
 	cout << " | Op:";
 	cout << std::setw(2) << std::hex << (int)opcode << endl;
 
-	if (pc == 0x3343 && ram[0x000d] > 0xFE)
-		cout << std::setw(4) << std::setfill('0') << std::hex << (int)ram[0x000d] << "============================================================" << endl;
+	//if (pc == 0x3343 && ram[0x000d] > 0xFE)
+	//	cout << std::setw(4) << std::setfill('0') << std::hex << (int)ram[0x000d] << "============================================================" << endl;
 
-	if (pc == 0x3345)
-		cout << "============================================================" << endl;
+	//if (pc == 0x3345)
+	//	cout << "============================================================" << endl;
+#endif
 
-
-	execute = !illegalOpcode;
+	prevpc = pc;
 }
 
 int main()
